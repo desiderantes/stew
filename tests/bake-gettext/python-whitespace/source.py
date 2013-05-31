994315663144e5ca6fdf49b493314411fc5d51f6
Robert Ancell <robert.ancell@canonical.com>
2013-05-31 20:05:53 +1200

    Make sure examples compile

--- tests/bake-gettext/python-whitespace/source.py
+++ tests/bake-gettext/python-whitespace/source.py
@@ -1,10 +1,10 @@
 import gettext
 
-w1 = _ ("whitespace1")
-w2 = _( "whitespace2")
-w3 = _("whitespace3" )
-w4 = _  (  "whitespace4"  )
-w5 = _
+w1 = gettext.gettext ("whitespace1")
+w2 = gettext.gettext( "whitespace2")
+w3 = gettext.gettext("whitespace3" )
+w4 = gettext.gettext  (  "whitespace4"  )
+w5 = gettext.gettext
 (
     "whitespace5"
 )
